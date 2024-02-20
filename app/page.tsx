3823066275fe9b74fe9e995b6724a4735b870a09
jpoechill <poamrong@rith.com>
2024-02-20 11:50:00 -0800

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,111 +2,157 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    // B58EBC
+    // f2d7e8
+    <main>
+      <div className="bg-[#ffd8fc] text-black px-10 py-6 flex justify-between">
+        <div>
+          <Image src="/maidforme.svg" alt="logo" width={220} height={60}></Image>
+        </div>
+        <div className="flex flex-row text-[16px] font-medium gap-10 mt-1">
+          <div>
+            About Us
+          </div>
+          <div>
+            Our Services
+          </div>
+          <div>
+            Contact Us
+          </div>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="video-container">
+        <video autoPlay muted loop>
+          <source src="vaccuum.mov" type="video/mp4" />
+        </video>
+        <div className="caption h-full text-white">
+          <div className="h-full bg-black bg-opacity-40 px-20 flex flex-col justify-start justify-center">
+            <div className="text-[40px] font-bold">
+              Professional Cleaning Solutions
+            </div>
+            <div>
+              Experience top-notch dusting, sanitizing, and vacuuming services with our professional cleaning team.
+            </div>
+            <div>
+              <button className="bg-[#ffd8fc] text-black rounded p-3 mt-4">
+                Contact Us
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <div className="grid grid-cols-6 my-10 gap-10 py-20 px-20">
+        <div className="col-span-2 justify-center flex">
+          <Image src="/thumb-04.png" className="rounded-xl" alt="logo" width={380} height={60}></Image>
+        </div>
+        <div className="col-span-4">
+          <div className="text-[40px] font-bold mb-8">
+            Spotless Excellence
+          </div>
+          <div>
+            Our cleaning services are designed to meet the highest standards of hygiene and cleanliness. We understand that every space has unique cleaning needs, and we tailor our services to meet those needs.
+            {/* <br /><br />
+            Our team of professional cleaners is trained to handle a variety of cleaning tasks, from basic dusting and vacuuming to deep cleaning and sanitization. */}
+            <br /><br />
+            Our goal is to provide you with a clean, healthy, and comfortable space that you can be proud of. We are committed to delivering exceptional cleaning services that exceed your expectations.
+          </div>
+        </div>
+      </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
+      <div className="bg-[#ECF8F7] py-20 px-[150px] flex flex-col">
+        <div className="text-[40px] text-center font-bold mb-4">
+          Our Services
+        </div>
+        <div className="flex justify-center mb-10">
+          <div className="flex max-w-[600px]  text-center">
+            We provide top-notch cleaning services tailored to meet the unique needs of businesses in the Cleaning Services industry. Our team of professionals is committed to delivering exceptional results.
+          </div>
+        </div>
+        <div className="flex flex-row gap-[50px] justify-between w-full mt-10 pb-10">
+          <div className="w-full">
+            <Image src="/thumb-05.png" className="rounded-xl" alt="thumb" width="400" height="400" />
+            <div className="min-h-[150px] text-[14px]">
+              <div className="font-bold text-[16px] my-5">Commercial Cleaning</div>
+              Our Commercial Cleaning service ensures a spotless, well-maintained business environment. We use eco-friendly products and advanced cleaning techniques for optimal results.
+            </div>
+          </div>
+          <div className="w-full">
+            <Image src="/thumb-02.png" className="rounded-xl" alt="thumb" width="400" height="400" />
+            <div className="min-h-[150px] text-[14px]">
+              <div className="font-bold text-[16px] my-5">Residential Cleaning</div>
+              Our Residential Cleaning service offers thorough, customized cleaning solutions for homes. We prioritize your comfort and satisfaction, ensuring every corner is spotless.
+            </div>
+          </div>
+          <div className="w-full">
+            <Image src="/thumb-03.png" className="rounded-xl" alt="thumb" width="400" height="400" />
+            <div className="min-h-[150px] text-[14px]">
+              <div className="font-bold text-[16px] my-5">Deep Cleaning</div>
+              Our Deep Cleaning service goes beyond the surface, targeting hard-to-reach areas for a comprehensive clean. We leave no stone unturned, ensuring your space is pristine.
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="text-center flex py-20 justify-center">
+        <div className="max-w-[900px]">
+          <div className="text-[40px] text-center font-bold mb-4">
+            Reach Out to Us for <br /> Exceptional Cleaning Services
+          </div>
+          <div>
+            Don't hesitate to contact us for top-notch cleaning services. <br />
+            Our team is ready to provide you with a spotless environment.
+          </div>
+          <div className="mt-[50px] text-left">
+            Name* <br />
+            <input type="text" className="border px-3 py-2 rounded-lg mt-2 w-full min-w-[500px]" /> <br /><br />
+            Email* <br />
+            <input type="text" className="border px-3 py-2 rounded-lg mt-2 w-full min-w-[500px]" /> <br /><br />
+            Phone Number* <br />
+            <input type="text" className="border px-3 py-2 rounded-lg mt-2 w-full min-w-[500px]" /> <br /><br />
+            Message* <br />
+            <input type="Subject" className="border px-3 py-2 rounded-lg mt-2 w-full min-w-[500px]" /> <br /><br />
+            <button className="bg-[#ECF8F7] w-full text-black rounded p-3 mt-4">
+              Let's talk!
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="bg-[#ffd8fc] text-black text-[14px] py-20 px-20">
+        <div className="grid grid-cols-6 gap-4">
+          <div className="col-span-3">
+            <span className="text-[16px]">
+              Sparkling cleanliness at your service, anytime, anywhere!
             </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+            <div className="flex flex-row gap-5 mt-3">
+              <svg className="h-8 w-8" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fill-rule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clip-rule="evenodd"></path></svg>
+              <svg className="h-8 w-8" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fill-rule="evenodd" d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z" clip-rule="evenodd"></path></svg>
+              <svg className="h-8 w-8" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path d="M19.589 6.686a4.793 4.793 0 0 1-3.77-4.245V2h-3.445v13.672a2.896 2.896 0 0 1-5.201 1.743l-.002-.001.002.001a2.895 2.895 0 0 1 3.183-4.51v-3.5a6.329 6.329 0 0 0-5.394 10.692 6.33 6.33 0 0 0 10.857-4.424V8.687a8.182 8.182 0 0 0 4.773 1.526V6.79a4.831 4.831 0 0 1-1.003-.104z" /></svg>
+            </div>
+          </div>
+          <div className="">
+            <span className="font-bold">
+              Menu
+            </span>
+            <br /><br />
+            About Us <br /><br />
+            Our Services <br /><br />
+            Contact Us
+          </div>
+          <div className="">
+            <span className="font-bold">
+              Contact Us
+            </span>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+            <br /><br />
+            hello@maidforme.co
+            <br /><br />
+            510-555-1234
+          </div>
+        </div>
+        <div className="mt-10 text-sm">
+          2024 © Maid For Me. All rights reserved.
+        </div>
       </div>
     </main>
   );
