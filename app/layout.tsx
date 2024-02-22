99ab0685255de8a15b3ab8a2b66a9fcae6a37b70
jpoechill <poamrong@rith.com>
2024-02-22 00:01:12 -0800

    update headers

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Maid For Me | maidforme.co | Professional Cleaning Services",
+  title: "Maid For Me | Professional Cleaning Services",
   description: "Maid For Me cleaning services are designed to meet the highest standards of hygiene and cleanliness. We understand that every space has unique cleaning needs, and we tailor our services to meet those needs.",
 };
 
