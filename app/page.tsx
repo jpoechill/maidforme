5bdfcfde01388eccc4b9653a0d2df84142a76ae8
jpoechill <poamrong@rith.com>
2024-04-19 11:29:29 -0700

    update favi

--- app/page.tsx
+++ app/page.tsx
@@ -48,6 +48,9 @@ export default function Home() {
             <span onClick={() => scrolltoHash('our-services')} className="hover:underline cursor-pointer">Our Services</span>
           </div>
           <div>
+            <span onClick={() => scrolltoHash('testimonials')} className="hover:underline cursor-pointer">Pricing</span>
+          </div>
+          <div>
             <span onClick={() => scrolltoHash('testimonials')} className="hover:underline cursor-pointer">Testimonials</span>
           </div>
           <div>
@@ -77,6 +80,9 @@ export default function Home() {
             </div> <br />
             <div onClick={() => { scrolltoHash('our-services-mobile'); setShowMenu(false) }} className="w-100 pb-5 border-b-2 hover:underline cursor-pointer">
               <span>Our Services</span>
+            </div> <br />
+            <div onClick={() => { scrolltoHash('our-services-mobile'); setShowMenu(false) }} className="w-100 pb-5 border-b-2 hover:underline cursor-pointer">
+              <span>Pricing</span>
             </div> <br />
             <div onClick={() => { scrolltoHash('testimonials'); setShowMenu(false) }} className="w-100 pb-5 border-b-2 hover:underline cursor-pointer">
               <span>Testimonials</span>
